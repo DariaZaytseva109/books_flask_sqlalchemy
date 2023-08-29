ca3651475600634a5fb676e4ac78a64ed6d41942
DariaZaytseva109 <dawazai@mail.ru>
2023-08-29 14:04:50 +0200

    Скорректировала вывод на главную страницу (+сортировка)

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
+from datetime import date
+
 from flask import Flask, render_template
 
 from model import Book, Genre, db
 
+API_ROOT = '/api/v1'
+BOOK_API_ROOT = API_ROOT + '/book/'
+GENRE_API_ROOT = API_ROOT + '/genre/'
+LIMIT = 3
 
 app = Flask(__name__)
-
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-
 db.init_app(app)
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-    db.session.add(Book(id=1, title='Мастер и Маргарита', author='М. Булгаков', genre_id=1))
+    db.session.add(Book(id=1, title='Мастер и Маргарита', author='М. Булгаков', genre_id=1, date_added = date(2022, 5, 5)))
     db.session.add(Book(id=2, title='Три товарища', author='Э.М. Ремарк', genre_id=1))
     db.session.add(Book(id=3, title='Герой нашего времени', author='М. Лермонтов', genre_id=1))
-    db.session.add(Book(id=4, title='Стихи', author='А. Пушкин', genre_id=2))
+    db.session.add(Book(id=4, title='Стихи', author='А. Пушкин', genre_id=2, date_added=date(2020, 1, 1)))
+    db.session.add(Book(id=5, title='Гордость и предубеждение', author='Д. Остин', genre_id=1, date_added=date(2021, 10, 5)))
+    db.session.add(Book(id=6, title='Стихи детям', author='А. Барто', genre_id=2, date_added=date(2023, 7, 9)))
     db.session.add(Genre(genre_id=1, genre='Роман'))
     db.session.add(Genre(genre_id=2, genre='Стихи'))
     db.session.commit()
 
-API_ROOT = '/api/v1'
-BOOK_API_ROOT = API_ROOT + '/book/'
-GENRE_API_ROOT = API_ROOT + '/genre/'
 
 
 
 @app.route(BOOK_API_ROOT)
 def list_books():
     '''view главной страницы приложения, который выводит 15 последних записей из Book в порядке создания - новые записи вверху списка'''
-    books = Book.query.all()
+    books = Book.query.order_by(Book.date_added.desc()).limit(LIMIT).all()
     return render_template('main_page_books.html', books=books)
 
 
--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 
@@ -7,9 +9,10 @@ db = SQLAlchemy()
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     title = db.Column(db.String)
-    author = db.Column(db.String)
+    author = db.Column(db.String(30))
     genre_id = db.Column(db.Integer, db.ForeignKey('genre.genre_id', ondelete='SET NULL'))
     genre_name = relationship('Genre', back_populates='books_of_genre')
+    date_added = db.Column(db.Date, default=date.today())
 
 
     def __repr__(self):
@@ -18,7 +21,7 @@ class Book(db.Model):
 
 class Genre(db.Model):
     genre_id = db.Column(db.Integer, primary_key=True, nullable=False)
-    genre = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String(30), nullable=False)
     books_of_genre = relationship('Book', back_populates='genre_name')
 
     def __repr__(self):
